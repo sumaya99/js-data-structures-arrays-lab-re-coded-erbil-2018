908d0bc03e744b105f50ddf472c95db06faff022
suvi shahoyi <sumaya.shahoyi@gmail.com>
2018-08-13 04:37:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
 // Write your solution here!
-let drivers = ["Milo", "Otis", "Garfield"];
-function () = { const drivers = []; }
+function destructivelyAppendDriver (name) {
+  drivers.push(name);
+}
+
+function destructivelyPrependDriver (name) {
+  drivers.unshift(name);
+}
+
+function destructivelyRemoveLastDriver () {
+  drivers.pop();
+}
+
+function destructivelyRemoveFirstDriver () {
+  drivers.shift();
+}
+
+function appendDriver (name) {
+  return [...drivers, name];
+}
+
+function prependDriver (name) {
+  return [name, ...drivers];
+}
+
+function removeFirstDriver () {
+  return drivers.slice(1);
+}
+
+function removeLastDriver () {
+  return drivers.slice(0, drivers.length - 1);
+}
